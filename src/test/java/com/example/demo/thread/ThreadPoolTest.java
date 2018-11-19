d37fa6136410652b5f99ac0df2dccd61ac32fea7
tangxin <tangxinqd@163.com>
2018-11-19 17:25:20 +0800

    线程池！！！

---
+++ src/test/java/com/example/demo/thread/ThreadPoolTest.java
@@ -0,0 +1,15 @@
+package com.example.demo.thread;
+
+import java.io.IOException;
+
+/**
+ * @Auther: TX
+ * @Date: 2018/11/19 0019 10:22
+ * @Description: 多线程
+ */
+public class ThreadPoolTest {
+
+    public static void  main(String [] args) throws IOException {
+
+    }
+}
