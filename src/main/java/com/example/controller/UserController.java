9288193cf50f361cd90f6c603f9c2b461feaa4ea
tangxin <tangxinqd@163.com>
2018-11-20 17:55:30 +0800

    统一异常处理完善！！！

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.example.controller;
 
 import com.example.domain.User;
+import com.example.exception.UserException;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,7 +18,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/user")
 public class UserController {
-
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @ApiOperation(value = "添加用户",notes = "根据User添加用户",httpMethod = "POST",produces = MediaType.APPLICATION_JSON_VALUE)
 //    @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
@@ -55,5 +58,13 @@ public class UserController {
         System.out.println(userName);
 
         return null;
+    }
+
+
+    @ApiOperation(value="查询用户",httpMethod = "GET")
+    @GetMapping("/{id}")
+    public  Object findOne(@PathVariable Long id) throws UserException {
+        logger.info("id:{}",id);
+        throw new UserException("errorCode","Do not find user!!");
     }
 }
