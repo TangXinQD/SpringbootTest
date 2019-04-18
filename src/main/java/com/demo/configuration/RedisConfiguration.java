c1a9c752cb66aac2d6de2203d79d81783e106884
txMayun <791995385@qq.com>
2019-04-18 21:17:18 +0800

    redisConfiguration and redisTemplate operation!

---
+++ src/main/java/com/demo/configuration/RedisConfiguration.java
@@ -0,0 +1,84 @@
+/**
+ * Copyright (C), 2015-2019, XXX有限公司
+ * FileName: RedisConfiguration
+ * Author:   TX
+ * Date:     2019/4/18 20:38
+ * Description: redis配置
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.demo.configuration;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ *
+ * 〈redis配置〉
+ *
+ * @author TX
+ * @create 2019/4/18
+ * @since 1.0.0
+ */
+@Configuration
+@EnableCaching
+public class RedisConfiguration {
+
+    @Bean
+    public JedisConnectionFactory redisConnectionFactory() {
+
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration("localhost", 6379);
+        return new JedisConnectionFactory(config);
+    }
+
+
+    @Bean(name = "redisTemplate")
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory factory) {
+        /*
+         * Redis 序列化器.
+         *
+         * RedisTemplate 默认的系列化类是 JdkSerializationRedisSerializer,用JdkSerializationRedisSerializer序列化的话,
+         * 被序列化的对象必须实现Serializable接口。在存储内容时，除了属性的内容外还存了其它内容在里面，总长度长，且不容易阅读。
+         *
+         * Jackson2JsonRedisSerializer 和 GenericJackson2JsonRedisSerializer，两者都能系列化成 json，
+         * 但是后者会在 json 中加入 @class 属性，类的全路径包名，方便反系列化。前者如果存放了 List 则在反系列化的时候如果没指定
+         * TypeReference 则会报错 java.util.LinkedHashMap cannot be cast to
+         */
+        RedisSerializer genericJackson2JsonRedisSerializer = new GenericJackson2JsonRedisSerializer();
+        RedisSerializer stringRedisSerializer = new StringRedisSerializer();
+
+        // 定义RedisTemplate，并设置连接工程
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+
+        // key 的序列化采用 StringRedisSerializer
+        redisTemplate.setKeySerializer(stringRedisSerializer);
+        redisTemplate.setHashKeySerializer(stringRedisSerializer);
+        // value 值的序列化采用 GenericJackson2JsonRedisSerializer
+        redisTemplate.setValueSerializer(genericJackson2JsonRedisSerializer);
+        redisTemplate.setHashValueSerializer(genericJackson2JsonRedisSerializer);
+        // 设置连接工厂
+        redisTemplate.setConnectionFactory(factory);
+
+        return redisTemplate;
+    }
+
+    @Bean
+    public CacheManager initRedisCacheManager(RedisConnectionFactory factory) {
+        RedisCacheManager.RedisCacheManagerBuilder builder = RedisCacheManager
+                .RedisCacheManagerBuilder.fromConnectionFactory(factory);
+        return builder.build();
+    }
+
+
+}
--- src/test/java/com/demo/demo/DemoApplicationTests.java
+++ src/test/java/com/demo/demo/DemoApplicationTests.java
@@ -1,16 +1,54 @@
 package com.demo.demo;
 
+import org.apache.commons.collections.map.HashedMap;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.*;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
+	@Autowired
+	private RedisTemplate redisTemplate;
+
 	@Test
 	public void contextLoads() {
+
+		ValueOperations value = redisTemplate.opsForValue();
+		HashOperations hashOperations = redisTemplate.opsForHash();
+		ListOperations listOperations = redisTemplate.opsForList();
+		SetOperations setOperations = redisTemplate.opsForSet();
+		ZSetOperations zSetOperations = redisTemplate.opsForZSet();
+
+		/*value.set("sKey","testString");
+		System.out.println(value.get("sKey"));*/
+		/*List<String> stringList = new ArrayList<String>();
+
+		stringList.add("1");
+		stringList.add("2");
+		stringList.add("3");
+		stringList.add("4");
+
+		listOperations.leftPush("list",stringList);*/
+
+		System.out.println(		value.setIfAbsent("sKey","setNX"));
+		System.out.println(		value.setIfAbsent("sNX","setNX"));
+
+		Map<String,Object> map = new HashedMap();
+
+		map.put("name","test");
+		map.put("age",132);
+
+		hashOperations.putAll("map",map);
+
 	}
 
 }
